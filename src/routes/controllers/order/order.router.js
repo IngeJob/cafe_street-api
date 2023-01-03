5e594d6757e0d348e69d9acdce1fc05cf578e1fc
Job <brayanjob_93@hotmail.com>
2023-01-03 09:02:16 -0500

    client commit

--- src/routes/controllers/order/order.router.js
+++ src/routes/controllers/order/order.router.js
@@ -4,6 +4,7 @@ const verifyToken = require('../../../middlewares/verifyToken.config')
 const orderRegister = require('./register/orderRegister.controller');
 const ordersByUser = require('./get/getOrdersByUser')
 
+
 order.route('/register').post( verifyToken, orderRegister )
 order.route('/ordersByUser').get( verifyToken, ordersByUser )
 
